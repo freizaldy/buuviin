bdc7430a514c1f84888d3cf38183aedce1361d2e
freizaldy <work.reizaldy@gmail.com>
2024-09-02 03:47:59 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey Salza! I love you." },
-    { author: "standinshd", quotes: "I love you, Salza." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday. Salza Nur Fatimah" },
-    { author: "standinshd", quotes: "I love you, Salza Nur Fatimah" },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "Freizaldy.", quotes: "Whatever happens, keep breathing" },
+    { author: "Farraaasss", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "Farrasreizaldy", quotes: "Hey Salza! I love you." },
+    { author: "Freizaldy", quotes: "I love you, Salza." },
+    { author: "Freizaldy", quotes: "I may not with you everyday, but I love you everyday. Salza Nur Fatimah" },
+    { author: "Freizaldy", quotes: "I love you, Salza Nur Fatimah" },
+    { author: "Freizaldy", quotes: "I'm sorry for loving you." },
 ];
