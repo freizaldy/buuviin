ca53897893f2df30847dcb9de8500e4f40fce505
freizaldy <work.reizaldy@gmail.com>
2024-09-02 03:38:33 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,9 +2,9 @@
 export default [
     { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
     { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
+    { author: "standinshd", quotes: "Hey Salza! I love you." },
+    { author: "standinshd", quotes: "I love you, Salza." },
+    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday. Salza Nur Fatimah" },
+    { author: "standinshd", quotes: "I love you, Salza Nur Fatimah" },
     { author: "standinshd", quotes: "I'm sorry for loving you." },
 ];
