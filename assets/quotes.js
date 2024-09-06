f18336bfa0372fa74d3bdb815d013c666cc7d053
freizaldy <work.reizaldy@gmail.com>
2024-09-06 23:01:42 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,7 +1,6 @@
 // prettier-ignore
 export default [
     { author: "Freizaldy.", quotes: "Whatever happens, keep breathing" },
-    { author: "Farraaasss", quotes: "Kamu mau hamil?" },
     { author: "Farrasreizaldy", quotes: "Hey Salza! I love you." },
     { author: "Freizaldy", quotes: "I love you, Salza." },
     { author: "Freizaldy", quotes: "I may not with you everyday, but I love you everyday. Salza Nur Fatimah" },
