9002cbcfbb8d6b63c5699348d98772f0ec9713a7
freizaldy <work.reizaldy@gmail.com>
2025-10-09 21:42:27 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,9 +1,9 @@
 // prettier-ignore
 export default [
     { author: "Reiza.", quotes: "Whatever happens, keep breathing" },
-    { author: "Reiza", quotes: "Hey Salza! I love you." },
-    { author: "Reiza", quotes: "I love you, Salza." },
-    { author: "Reiza", quotes: "I may not with you everyday, but I love you everyday. Salza Nur Fatimah" },
-    { author: "Reiza", quotes: "I love you, Salza Nur Fatimah" },
+    { author: "Reiza", quotes: "Hey Alma! I love you." },
+    { author: "Reiza", quotes: "I love you, Alma." },
+    { author: "Reiza", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "Reiza", quotes: "I love you, Almaaaaaaaaaaaaaaa" },
     { author: "Reiza", quotes: "I'm sorry for loving you." },
 ];
